6f6a17a73d3cc1560dc26156e6b3cf7de9900656
adi <adi@ancilla.com>
2018-02-22 16:22:49 +0100

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd client
-ng build --prod -pr false
+ng build --prod -pr false  2> /dev/null
 cd ..
 
-rm -rf ./deploy
+rm -rf ./deploy  2> /dev/null
 mkdir deploy
 
 cp -r ./server/* ./deploy
@@ -13,7 +13,10 @@ cp -r ./client/dist ./deploy
 rm ./deploy/yarn.lock
 rm -rf ./deploy/node_modules
 
-rm ./deploy.zip
+rm ./deploy.zip  2> /dev/null
 
 cd deploy
 zip -rq ../deploy.zip .
+
+cd ..
+rm -rf ./deploy
